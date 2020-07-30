28ab8136205ddf5b8f4a73a26dfe8a7da89af04f
bigbang <litter_k@163.com>
2020-07-30 16:25:20 +0800

    修修补补

--- com.tc.demo/src/com/tc/demo/util/TCUtils.java
+++ com.tc.demo/src/com/tc/demo/util/TCUtils.java
@@ -1,4 +1,21 @@
 package com.tc.demo.util;
 
+import java.util.Map;
+
+import com.teamcenter.rac.kernel.TCComponentForm;
+import com.teamcenter.rac.kernel.TCComponentItemRevision;
+import com.teamcenter.rac.stylesheet.InterfacePropertyComponent;
+
 public class TCUtils {
+
+	public static TCComponentForm getMasterForm(TCComponentItemRevision rev) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Map<String, String> getWidgetValue(Map<String, InterfacePropertyComponent> code_props) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
